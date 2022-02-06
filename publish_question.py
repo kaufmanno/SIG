77d86e7f72963090f256311abdb9059285e2d53b
su530201 <olivier.kaufmann@umons.ac.be>
2022-02-06 17:54:08 +0100

    Moves tools

--- publish_question.py
+++ publish_question.py
@@ -219,8 +219,8 @@ def commit_and_pull_repo(repo):
 def clean_path(course):
     assert_on_branch('questions')
     if verbose:
-        print(f'copying tools to {course}...')
-    execute(f'cp tools/* ./{course}')
+        print(f'Moving tools to {course}...')
+    execute(f'git mv tools/* ./{course}')
     if verbose:
         print(f'Moving files from {course}...')
     execute('find . -maxdepth 1 -type f -exec git rm {} \\;')
